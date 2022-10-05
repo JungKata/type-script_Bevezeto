873a179e0af7ad5c73a17c4295423b0b6cf1a9ce
JungKata <97729333+JungKata@users.noreply.github.com>
2022-10-05 14:59:30 +0200

    szamol

--- src/index.js
+++ src/index.js
@@ -25,3 +25,46 @@ let u = users[1];
 for (let e of users) {
     e.kiir();
 }
+let usernames = ['str1', 'user2'];
+let userHalmaz = new Set();
+let nevUser = new Map();
+let nevUser2 = new Map();
+nevUser2.set("usernev", new User_1.User("usernev", 33));
+//nevUser2.get("usernev").kiir();
+/*
+function osszeg(t : number[] | string[]){
+    let sum : string | number = 0;;
+    for(let e of t){
+        sum += e;
+    }
+    return sum;
+}
+*/
+function osszeg(t) { }
+;
+console.log(osszeg([1, 45666, 43345]));
+console.log(osszeg(['a', 'b', 'c']));
+function IDkiir(id) {
+    if (typeof id === 'number') {
+        console.log(id);
+    }
+    else {
+        console.log(id.toUpperCase());
+    }
+}
+IDkiir(12);
+IDkiir("asdf423");
+let elem = document.getElementById('asdf');
+if (elem !== null) {
+    console.log(elem.textContent);
+}
+console.log(elem === null || elem === void 0 ? void 0 : elem.textContent);
+function szamol() {
+    let szam = parseInt(document.getElementById('szam').value);
+    document.getElementById('kimenet').textContent = negyzet(szam).toString();
+}
+function init() {
+    var _a;
+    (_a = document.getElementById('szam')) === null || _a === void 0 ? void 0 : _a.addEventListener('input', szamol);
+}
+document.addEventListener('DOMContentLoaded', init);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 
 
+import internal from "stream";
 import{User} from "./User";
 
 function negyzet(szam : number) : number{
@@ -31,3 +32,55 @@ let u = users[1];
 for(let e of users){
     e.kiir();
 }
+
+let usernames : Array<string> = ['str1', 'user2'];
+let userHalmaz : Set<User> = new Set();
+
+let nevUser : Map<string, User> = new Map();
+let nevUser2 = new Map<string, User>();
+
+nevUser2.set("usernev", new User("usernev", 33));
+//nevUser2.get("usernev").kiir();
+
+/*
+function osszeg(t : number[] | string[]){
+    let sum : string | number = 0;;
+    for(let e of t){
+        sum += e;
+    }
+    return sum;
+}
+*/
+function osszeg(t : number[] | string[]){};
+console.log(osszeg([1 ,45666 , 43345]));
+    console.log(osszeg(['a', 'b', 'c']));
+
+function IDkiir(id : number | string)
+{
+    if(typeof id ==='number')
+    {
+        console.log(id);
+    }else{
+        console.log(id.toUpperCase());
+    }
+}
+
+IDkiir(12);
+IDkiir("asdf423");
+
+let elem = document.getElementById('asdf');
+if(elem !== null){
+console.log(elem.textContent);
+}
+console.log(elem?.textContent)
+
+function szamol(){
+    let szam = parseInt((document.getElementById('szam') as HTMLInputElement).value);
+    (document.getElementById('kimenet') as HTMLElement).textContent = negyzet(szam).toString();
+}
+
+function init(){
+document.getElementById('szam')?.addEventListener('input', szamol);
+}
+
+document.addEventListener('DOMContentLoaded', init)
